67b0b65d587a84bb8b1a5e015b670e2cee325f34
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-06-14 15:43:13 -0400

    Accept user answer and calculate difference

--- src/Components/TriviaDisplay.js
+++ src/Components/TriviaDisplay.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {fetchTrivia} from '../api/fetchTrivia'
 import '../styles/Trivia.css'
 
 const TriviaDisplay = () => {
   const [currentQuestion, setCurrentQuestion] = useState({})
+  const [showAnswer, setShowAnswer] = useState(false)
+  const [difference, setDifference] = useState (null)
+  const [userGuess, setUserGuess] = useState (null)
 
   const questionify = text => {
     let spaceIndex = text.indexOf(' ');
-    let questionString = "What" + text.slice(spaceIndex);
+    let questionString = "What" + text.slice(spaceIndex, -1) + "?";
     return questionString;
   }
 
@@ -17,6 +20,26 @@ const TriviaDisplay = () => {
     setCurrentQuestion(questionData)
   }
 
+  const submitAnswer = (event) => {
+    event.preventDefault();
+    document.getElementById('submit').disabled = true;
+    setShowAnswer(true);
+    let userAnswer = document.getElementById('answer').value
+    document.getElementById('answer').value = null;
+    setDifference(Math.abs(userAnswer - currentQuestion.number));
+    setUserGuess(userAnswer)
+    setShowAnswer(true);
+  }
+
+  useEffect(()=> {
+    fetchTrivia()
+    .then(questionData=> {
+      questionData.text = questionify(questionData.text);
+      setCurrentQuestion(questionData)
+    })
+    }
+    ,[]
+  )
   return (
     <div id="trivia-display">
       <div>
@@ -24,12 +47,19 @@ const TriviaDisplay = () => {
       </div>
       {currentQuestion.text ?
       <div>
-        <label htmlFor="answer">Answer</label> 
-        <input id="answer" type="number"/>
+        <form onSubmit={submitAnswer}>
+          <label htmlFor="answer">Enter your answer</label> 
+          <input id="answer" type="number"/>
+          <input id="submit" type="submit"/>
+        </form>
       </div>
       : null }
-     
-      <button onClick={getQuestion}>Get trivia question</button>
+      <div>
+        {showAnswer ? 
+        <p>You entered {userGuess}. The correct answer is {currentQuestion.number}. That's a difference of {difference}.</p> 
+        : null }
+      </div>
+      <button onClick={getQuestion}>Next question</button>
     </div>
   )
 }
